74e0e15d55f574d13924873092aac685c94cea52
hieuttblackpink <trantronghieu369963@gmail.com>
2024-12-11 16:44:33 +0700

    add exercise 3 and 4 in python, fix octave Ex4_1

--- Dữ liệu bài tập/Buổi 04 (07-12-C)/Ex4_1.m
+++ Dữ liệu bài tập/Buổi 04 (07-12-C)/Ex4_1.m
@@ -30,21 +30,21 @@ FS = 15; %Font-size of caption
 %b. Negative transformations
 %plot the transformation function
 ##figure(1), clf;
-##x = uint8(255: -1: 0);
+##y = uint8(255: -1: 0);
 ##plot(y, 'LineWidth', 1.5);
 ##xlim([0 255]);
 ##ylim([0 255]);
 ##grid on;
 ##
 ##%perform the identity transformation
-##I2 = x(I + 1);
+##I2 = y(I + 1);
 ##figure(2), clf;
 ##subplot(1, 2, 1), imshow(I), title('Original image', "fontsize", FS);
 ##subplot(1, 2, 2), imshow(I2), title('Negative', "fontsize", FS);
 
-######################################################
-##%c. Logarithmic transformations
-##%plot the transformation function
+########################################################
+####%c. Logarithmic transformations
+####%plot the transformation function
 ##figure(1), clf;
 ##x = uint8(0: 255);
 ##c = 255 / log(256);
